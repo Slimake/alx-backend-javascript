bcd15e8a6994a6a6434c4a06b2623e7763f405f5
slimake <marcusitovie@gmail.com>
2024-10-01 12:21:00 +0100

    Change const to let

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-    task
-    task2
+    let task = true;
+    let task2 = false;
+
+    task;
+    task2;
   }
 
   return [task, task2];
