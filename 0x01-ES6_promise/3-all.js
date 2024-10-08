dbc4cd7240e2f865935222edbad8f1fc704f336f
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-08 23:53:24 +0100

    Return an array of promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,6 +4,7 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(values[0].body, values[1].firstName, values[1].lastName);
+      return values;
     })
     .catch(() => console.error('Signup system offline'));
 }
