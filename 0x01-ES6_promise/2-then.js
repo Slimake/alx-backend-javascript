6ec01296f5ba5ec676412f850207e2e0138e1e5c
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-08 22:05:52 +0100

    Modify code

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'success' });
+  return promise
+    .then(() => {
       console.log('Got a response from the API');
-    } else {
-      reject(new Error());
-    }
-  });
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => new Error());
 }
