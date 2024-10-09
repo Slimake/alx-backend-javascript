412818c211da502da84c6977d1bdfd53539d417c
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-09 12:52:51 +0100

    Format code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,8 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((response) => response.forEach((resp) => ({
-      status: resp.status,
-      value: resp.status === 'fulfilled' ? resp.value : resp.reason,
-    })));
+    .then((response) => response.map((resp) => (
+      {
+        status: resp.status,
+        value: resp.status === 'fulfilled' ? resp.value : resp.reason,
+      }
+    )));
 }
