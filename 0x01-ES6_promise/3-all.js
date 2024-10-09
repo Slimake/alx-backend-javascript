0489346440c96e8ac66021feb527d1bc80bb0c60
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-09 11:12:36 +0100

    return a string

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,6 +4,10 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
       console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+      return `${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`;
     })
-    .catch(() => console.error('Signup system offline'));
+    .catch(() => {
+      console.error('Signup system offline');
+      return 'Signup system offline';
+    });
 }
