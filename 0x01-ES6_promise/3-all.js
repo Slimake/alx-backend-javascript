06232c8d964a1a44da27d8ac67734fe9e3a794ec
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-09 14:08:25 +0100

    Remove all return keyword

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,10 +4,8 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
       console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
-      return `${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`;
     })
     .catch(() => {
-      console.error('Signup system offline');
-      return 'Signup system offline';
+      console.log('Signup system offline');
     });
 }
