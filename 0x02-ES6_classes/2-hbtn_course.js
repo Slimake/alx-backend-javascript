7c4c7a08a943ac6ec7886b0ec4e6610aec3b5b64
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-12 20:23:06 +0100

    Implement a class named HolbertonCourse

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,58 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (typeof students !== 'object') {
+      throw new TypeError('students must be an array of strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Geter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students
+  set students(value) {
+    if (typeof value !== 'object') {
+      throw new TypeError('students must be an array of strings');
+    }
+    this._students = value;
+  }
+}
