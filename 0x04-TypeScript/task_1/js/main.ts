9e05980ff6d4e4d32b8341f43e9204b068560c0c
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-12 01:55:57 +0100

    Describe a class through interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -46,3 +46,34 @@ const teacherName : printTeacherFunction = {
 }
 
 console.log(printTeacher(teacherName.firstName, teacherName.lastName));
+
+
+interface Student {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentConstructor {
+  new(firstName: string, lastName: string): Student;
+}
+
+class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  public workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  public displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student: Student = new StudentClass('Sally', 'Mon');
+console.log(student);
