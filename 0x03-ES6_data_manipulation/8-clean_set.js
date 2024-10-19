b88c16d7df6613afff86bae8e7d2fda50cd4508d
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-19 21:13:27 +0100

    Refactor code

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,9 +4,7 @@ export default function cleanSet(set, startString) {
   }
 
   const filteredArr = [...set]
-    .filter((elem) => (
-      typeof elem === 'string' && elem.startsWith(startString)
-    ))
+    .filter((elem) => typeof elem === 'string' && elem.startsWith(startString))
     .map((elem) => elem.slice(startString.length));
 
   return filteredArr.join('-');
