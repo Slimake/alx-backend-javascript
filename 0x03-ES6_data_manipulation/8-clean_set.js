d209cc101ae93e8d2ab4e3081ea372b10c5b378f
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-19 20:51:08 +0100

    Check if set element is a string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const filteredArr = [...set]
-    .filter((elem) => startString !== '' && elem.startsWith(startString))
+    .filter((elem) => typeof elem === 'string' && startString !== '' && elem.startsWith(startString))
     .map((elem) => elem.slice(startString.length));
 
   return filteredArr.join('-');
