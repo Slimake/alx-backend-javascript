90569c08583c9752c1e5802a1635bcd81a69a5be
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-19 21:10:52 +0100

    Add check for startString

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,12 @@
 export default function cleanSet(set, startString) {
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+
   const filteredArr = [...set]
-    .filter((elem) => typeof elem === 'string' && startString !== '' && elem.startsWith(startString))
+    .filter((elem) => (
+      typeof elem === 'string' && elem.startsWith(startString)
+    ))
     .map((elem) => elem.slice(startString.length));
 
   return filteredArr.join('-');
