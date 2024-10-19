0469d94649efa9a5eff39c31c91e3ed4cdf2c9e9
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-19 13:05:42 +0100

    Fix: if block logic

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  try {
-    const buffer = new ArrayBuffer(length);
-    const in8Array = new Int8Array(buffer);
-    in8Array[position] = value;
-    const dataView = new DataView(buffer);
-    return dataView;
-  } catch (err) {
+  if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
+
+  const buffer = new ArrayBuffer(length);
+  const in8Array = new Int8Array(buffer);
+  in8Array[position] = value;
+  const dataView = new DataView(buffer);
+  return dataView;
 }
