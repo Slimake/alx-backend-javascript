254f2bb091d4ea79c4f72afa5372f622e3c4d481
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-11-26 19:50:24 +0100

    Refactor code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
 const message = 'Welcome to Holberton School, what is your name?';
 console.log(message);
 
-process.stdin.on('readable', function() {
-  const student_name = process.stdin.read();
-  if (student_name !== null) {
-    process.stdout.write(`Your name is: ${student_name}`)
+process.stdin.on('readable', () => {
+  const studentName = process.stdin.read();
+  if (studentName !== null) {
+    process.stdout.write(`Your name is: ${studentName}`);
   }
-  process.stdin.resume();
+  process.stdin.resume()
+});
+
+process.stdin.on('end', () => {
   // Display the closing message
   console.log('This important software is now closing');
-
-  // Exit the process
-  process.exit();
 });
