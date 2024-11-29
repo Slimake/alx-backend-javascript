d6a04f793aab0d42eccf8d28fb6575092e7660a1
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-11-29 12:57:39 +0100

    Test for calculateNumber function

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumberWithType', function() {
+  it('test suite for when type is SUM',  function () {
+    assert.equal(calculateNumber('SUM', 1.4, 4.3), 5);
+  });
+  it('test suite for when type is SUBTRACT',  function () {
+    assert.equal(calculateNumber('SUBTRACT', 1.5, 5.4), 3);
+  });
+  it('test suite for when type is DIVIDE',  function () {
+    assert.equal(calculateNumber('DIVIDE', 7.8, 4.2), 2);
+  });
+  it('test suite for when type is DIVIDE and third argument is 0',  function () {
+    assert.equal(calculateNumber('DIVIDE', 4.2, 0), 'Error');
+  });
+});
