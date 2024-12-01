56c95d8c052f81a4179ada5b3c1f707aef5541e3
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-12-01 20:38:13 +0100

    Add more tests

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,7 +2,17 @@ const expect = require('chai').expect;
 const request = require('request');
 
 describe('Index page', () => {
-  it('correct status code', () => {
+  it('correct status code for home route', () => {
+    request('http://localhost:7865', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+    });
+  });
+  it('correct result', () => {
+    request('http://localhost:7865', (err, res, body) => {
+      expect(res.body).to.equal('Welcome to the payment system');
+    });
+  });
+  it('correct status code for cart route', () => {
     request('http://localhost:7865/cart/12', (err, res, body) => {
       expect(res.statusCode).to.equal(200);
     });
