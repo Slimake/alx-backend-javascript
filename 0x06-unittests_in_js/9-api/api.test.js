1e83df4ef3d637a87f797f3007e1d81b7e2c48f3
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-12-01 20:16:33 +0100

    Test for api.js

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,20 @@
+const expect = require('chai').expect;
+const request = require('request');
+
+describe('Index page', () => {
+  it('correct status code', () => {
+    request('http://localhost:7865/cart/12', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+    });
+  });
+  it('incorrect status code when param is not an integer', () => {
+    request('http://localhost:7865/cart/hello', (err, res, body) => {
+      expect(res.statusCode).to.equal(404);
+    });
+  });
+  it('correct result', () => {
+    request('http://localhost:7865/cart/12', (err, res, body) => {
+      expect(res.body).to.equal('Payment methods for cart 12');
+    });
+  });
+});
